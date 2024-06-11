d4140894affe9609df4f6fe5f9c60e0de85e8f43
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2024-06-11 09:50:17 +0100

    Fixing the virtualenv path in elpy.

--- modes/elpy-mode.el
+++ modes/elpy-mode.el
@@ -5,7 +5,7 @@
   :config
   (setq elpy-rpc-backend "jedi"
         elpy-rpc-python-command "python"
-        elpy-rpc-virtualenv-path 'current
+        elpy-rpc-virtualenv-path "~/.pyenv/shims/python"
         py-autopep8-options '("--ignore E402"))
   :hook
   ;; (elpy-mode . py-autopep8-enable-on-save)
