fae83f31293ef46b5d76ab0f8ebbe3691b18c638
Guilherme Schardong <schardong.gg@gmail.com>
2024-07-15 20:39:40 +0100

    Updating auctex mode.

--- modes/auctex-mode.el
+++ modes/auctex-mode.el
@@ -1,72 +1,33 @@
 (add-hook 'latex-mode-hook 'reftex-mode)
 
-(use-package tex-site
+(use-package tex
+  :ensure auctex
+  :defer 4
   :config
   (setq TeX-auto-save t
         TeX-parse-self t
-        TeX-PDF-mode t)
+        TeX-PDF-mode t
+        reftex-cite-format 'bibtex
+        reftex-plug-into-AUCTeX t)
   (setq-default TeX-master nil
-                TeX-command-extra-options "-shell-escape"))
-
-(use-package reftex
-  :ensure t
-  :after (auctex)
-  :config
-  (setq reftex-plug-into-AUCTeX t
-        ;; reftex-default-bibliography '("~/my-bib-file.bib")
-        reftex-enable-partial-scanning t))
-
-;;(define-key tex-mode-map (kbd "C-c C-e") 'TeX-command-run-all)
-;;(define-key tex-mode-map (kbd "C-c C-c") 'reftex-citation)
-;;(define-key tex-mode-map (kbd "C-c C-l") 'reftex-label)
-
-
-;;(use-package tex-site
-;;  :ensure auctex
-;;  :defer t
-;;  :mode ("\\.tex\\'" . latex-mode))
-  ;; :config
-  ;; (setq-default TeX-master nil
-  ;;               TeX-auto-save t
-  ;;               TeX-save-query nil
-  ;;               TeX-parse-self t
-  ;;               reftex-plug-into-AUCTeX t)
-  ;; (add-hook 'LaTeX-mode-hook
-  ;;           (lambda ()
-  ;;             (setq TeX-PDF-mode t)
-  ;;             (setq TeX-source-correlate-method 'synctex)
-  ;;             (setq TeX-source-correlate-start-server t)))
-  ;; :hook
+                TeX-command-extra-options "-shell-escape")
+  :hook
+  (LaTeX-mode . reftex-mode)
   ;; (LaTeX-mode . TeX-PDF-mode)
   ;; (LaTeX-mode . company-mode)
   ;; (LaTeX-mode . flyspell-mode)
   ;; (LaTeX-mode . flycheck-mode)
   ;; (LaTeX-mode . LaTeX-math-mode)
-  ;; (LaTeX-mode . turn-on-reftex)
-  ;; (LaTeX-mode . turn-on-cdlatex)
   ;; (LaTeX-mode . rainbow-delimiters)
   ;; (LaTeX-mode . smartparens)
-  ;; (LaTeX-mode . reftex-isearch-minor))
+  ;; (LaTeX-mode . reftex-isearch-minor)
+  )
 
-;; Update PDF buffers after successful LaTeX runs
-;; (add-hook 'TeX-after-TeX-LaTeX-command-finished-hook
-;;            #'TeX-revert-document-buffer)
-
-;; to use pdfview with auctex
-;; (add-hook 'LaTeX-mode-hook 'pdf-tools-install)
-
-;; to use pdfview with auctex
-;; (setq TeX-view-program-selection '((output-pdf "pdf-tools"))
-;;        TeX-source-correlate-start-server t)
-;; (setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
-
-;; (use-package reftex
-;;   :ensure t
-;;   :after (auctex)
-;;   :commands turn-on-reftex
-;;   :config
-;;   (setq reftex-plug-into-AUCTeX t
-;;         reftex-cite-prompt-optional-args t))
+(use-package reftex
+  :ensure t
+  :config
+  (setq reftex-plug-into-AUCTeX t
+        reftex-enable-partial-scanning t))
 
 (use-package pdf-tools
   :ensure t
